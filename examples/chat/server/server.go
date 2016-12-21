aafe0f6651677be4685146e51ca1f6a3fbffee06
zhoulindong <zhoulindong@qima-inc.com>
2016-12-21 11:40:29 +0800

    format, change log, change MAX_CONNECTIONS

--- examples/chat/server/server.go
+++ examples/chat/server/server.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-  "fmt"
-  "runtime"
-  "github.com/leesper/tao"
-  "github.com/leesper/tao/examples/chat"
-  "github.com/leesper/holmes"
+	"fmt"
+	"github.com/reechou/holmes"
+	"github.com/leesper/tao"
+	"github.com/leesper/tao/examples/chat"
+	"runtime"
 )
 
 type ChatServer struct {
-  tao.Server
+	tao.Server
 }
 
 func NewChatServer(addr string) *ChatServer {
-  return &ChatServer {
-    tao.NewTCPServer(addr),
-  }
+	return &ChatServer{
+		tao.NewTCPServer(addr),
+	}
 }
 
 func main() {
-  runtime.GOMAXPROCS(runtime.NumCPU())
-  defer holmes.Start().Stop()
-  tao.MonitorOn(12345)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	defer holmes.Start().Stop()
+	tao.MonitorOn(12345)
 
-  tao.Register(chat.CHAT_MESSAGE, chat.DeserializeChatMessage, chat.ProcessChatMessage)
+	tao.Register(chat.CHAT_MESSAGE, chat.DeserializeChatMessage, chat.ProcessChatMessage)
 
-  chatServer := NewChatServer(fmt.Sprintf("%s:%d", "0.0.0.0", 18341))
+	chatServer := NewChatServer(fmt.Sprintf("%s:%d", "0.0.0.0", 18341))
 
-  chatServer.SetOnConnectCallback(func(conn tao.Connection) bool {
-    holmes.Info("%s", "On connect")
-    return true
-  })
+	chatServer.SetOnConnectCallback(func(conn tao.Connection) bool {
+		holmes.Info("%s", "On connect")
+		return true
+	})
 
-  chatServer.SetOnErrorCallback(func() {
-    holmes.Info("%s", "On error")
-  })
+	chatServer.SetOnErrorCallback(func() {
+		holmes.Info("%s", "On error")
+	})
 
-  chatServer.SetOnCloseCallback(func(conn tao.Connection) {
-    holmes.Info("%s", "Closing chat client")
-  })
+	chatServer.SetOnCloseCallback(func(conn tao.Connection) {
+		holmes.Info("%s", "Closing chat client")
+	})
 
-  chatServer.Start()
+	chatServer.Start()
 }
--- message.go
+++ message.go
@@ -1,17 +1,17 @@
 package tao
 
 import (
-  "bytes"
-  "encoding/binary"
-  "fmt"
-  "io"
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
 
-  "github.com/leesper/holmes"
+	"github.com/reechou/holmes"
 )
 
 // 0 is the preserved heart beat message number, you can define your own.
 const (
-  HEART_BEAT = 0
+	HEART_BEAT = 0
 )
 
 // handlerFunc handles the business logic for some type of Message.
@@ -22,19 +22,19 @@ type unmarshalFunc func([]byte) (Message, error)
 
 // messageFunc is the collection of unmarshal and handle functions about Message.
 type messageFunc struct {
-  handler handlerFunc
-  unmarshaler unmarshalFunc
+	handler     handlerFunc
+	unmarshaler unmarshalFunc
 }
 
 // messageRegistry is the inner registry of all message related unmarshal and handle functions.
 var (
-  buf *bytes.Buffer
-  messageRegistry map[int32]messageFunc
+	buf             *bytes.Buffer
+	messageRegistry map[int32]messageFunc
 )
 
 func init() {
-  messageRegistry = map[int32]messageFunc{}
-  buf = new(bytes.Buffer)
+	messageRegistry = map[int32]messageFunc{}
+	buf = new(bytes.Buffer)
 }
 
 // Register registers the unmarshal and handle functions for msgType.
@@ -43,182 +43,182 @@ func init() {
 // set a default one by calling SetOnMessageCallback.
 // If Register being called twice on one msgType, it will panics.
 func Register(msgType int32, unmarshaler func([]byte) (Message, error), handler func(Context, Connection)) {
-  if _, ok := messageRegistry[msgType]; ok {
-    panic(fmt.Sprintf("trying to register message %d twice", msgType))
-  }
+	if _, ok := messageRegistry[msgType]; ok {
+		panic(fmt.Sprintf("trying to register message %d twice", msgType))
+	}
 
-  messageRegistry[msgType] = messageFunc{
-    unmarshaler: unmarshaler,
-    handler: handler,
-  }
+	messageRegistry[msgType] = messageFunc{
+		unmarshaler: unmarshaler,
+		handler:     handler,
+	}
 }
 
 // GetUnmarshaler returns the corresponding unmarshal function for msgType.
 func GetUnmarshaler(msgType int32) unmarshalFunc {
-  entry, ok := messageRegistry[msgType]
-  if !ok {
-    return nil
-  }
-  return entry.unmarshaler
+	entry, ok := messageRegistry[msgType]
+	if !ok {
+		return nil
+	}
+	return entry.unmarshaler
 }
 
 // GetHandler returns the corresponding handler function for msgType.
 func GetHandler(msgType int32) handlerFunc {
-  entry, ok := messageRegistry[msgType]
-  if !ok {
-    return nil
-  }
-  return entry.handler
+	entry, ok := messageRegistry[msgType]
+	if !ok {
+		return nil
+	}
+	return entry.handler
 }
 
 // Message represents the structured data that can be handled.
 type Message interface {
-  MessageNumber() int32
-  Serialize() ([]byte, error)
+	MessageNumber() int32
+	Serialize() ([]byte, error)
 }
 
 // Context is the context info for every handler function.
 // Handler function handles the business logic about message.
 // We can find the client connection who sent this message by netid and send back responses.
-type Context struct{
-  message Message
-  netid int64
+type Context struct {
+	message Message
+	netid   int64
 }
 
 func NewContext(msg Message, id int64) Context {
-  return Context{
-    message: msg,
-    netid: id,
-  }
+	return Context{
+		message: msg,
+		netid:   id,
+	}
 }
 
-func (ctx Context)Message() Message {
-  return ctx.message
+func (ctx Context) Message() Message {
+	return ctx.message
 }
 
-func (ctx Context)Id() int64 {
-  return ctx.netid
+func (ctx Context) Id() int64 {
+	return ctx.netid
 }
 
 // HeartBeatMessage for long-term connection keeping alive.
 type HeartBeatMessage struct {
-  Timestamp int64
+	Timestamp int64
 }
 
 func (hbm HeartBeatMessage) Serialize() ([]byte, error) {
-  buf.Reset()
-  err := binary.Write(buf, binary.BigEndian, hbm.Timestamp)
-  if err != nil {
-    return nil, err
-  }
-  return buf.Bytes(), nil
+	buf.Reset()
+	err := binary.Write(buf, binary.BigEndian, hbm.Timestamp)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
 func (hbm HeartBeatMessage) MessageNumber() int32 {
-  return HEART_BEAT
+	return HEART_BEAT
 }
 
 func DeserializeHeartBeatMessage(data []byte) (message Message, err error) {
-  var timestamp int64
-  if data == nil {
-    return nil, ErrorNilData
-  }
-  buf := bytes.NewReader(data)
-  err = binary.Read(buf, binary.BigEndian, &timestamp)
-  if err != nil {
-    return nil, err
-  }
-  return HeartBeatMessage{
-    Timestamp: timestamp,
-  }, nil
+	var timestamp int64
+	if data == nil {
+		return nil, ErrorNilData
+	}
+	buf := bytes.NewReader(data)
+	err = binary.Read(buf, binary.BigEndian, &timestamp)
+	if err != nil {
+		return nil, err
+	}
+	return HeartBeatMessage{
+		Timestamp: timestamp,
+	}, nil
 }
 
 func ProcessHeartBeatMessage(ctx Context, conn Connection) {
-  heartBeatMsg := ctx.Message().(HeartBeatMessage)
-  conn.SetHeartBeat(heartBeatMsg.Timestamp)
+	heartBeatMsg := ctx.Message().(HeartBeatMessage)
+	conn.SetHeartBeat(heartBeatMsg.Timestamp)
 }
 
 // Codec is the interface for message coder and decoder.
 // Application programmer can define a custom codec themselves.
 type Codec interface {
-  Decode(Connection) (Message, error)
-  Encode(Message) ([]byte, error)
+	Decode(Connection) (Message, error)
+	Encode(Message) ([]byte, error)
 }
 
 // Format: type-length-value |4 bytes|4 bytes|n bytes <= 8M|
-type TypeLengthValueCodec struct {}
+type TypeLengthValueCodec struct{}
 
 // Decode decodes the bytes data into Message
-func (codec TypeLengthValueCodec)Decode(c Connection) (Message, error) {
-  byteChan := make(chan []byte)
-  errorChan := make(chan error)
-  var err error
+func (codec TypeLengthValueCodec) Decode(c Connection) (Message, error) {
+	byteChan := make(chan []byte)
+	errorChan := make(chan error)
+	var err error
 
-  go func(bc chan []byte, ec chan error) {
-    typeData := make([]byte, NTYPE)
-    _, err = io.ReadFull(c.GetRawConn(), typeData)
-    if err != nil {
-      ec<- err
-      return
-    }
-    bc<- typeData
-  }(byteChan, errorChan)
+	go func(bc chan []byte, ec chan error) {
+		typeData := make([]byte, NTYPE)
+		_, err = io.ReadFull(c.GetRawConn(), typeData)
+		if err != nil {
+			ec <- err
+			return
+		}
+		bc <- typeData
+	}(byteChan, errorChan)
 
-  var typeBytes []byte
+	var typeBytes []byte
 
-  select {
-  case <-c.GetCloseChannel():
-    return nil, ErrorConnClosed
+	select {
+	case <-c.GetCloseChannel():
+		return nil, ErrorConnClosed
 
-  case err = <-errorChan:
-    return nil, err
+	case err = <-errorChan:
+		return nil, err
 
-  case typeBytes = <-byteChan:
-    typeBuf := bytes.NewReader(typeBytes)
-    var msgType int32
-    if err = binary.Read(typeBuf, binary.LittleEndian, &msgType); err != nil {
-      return nil, err
-    }
-    lengthBytes := make([]byte, NLEN)
-    _, err = io.ReadFull(c.GetRawConn(), lengthBytes)
-    if err != nil {
-      return nil, err
-    }
-    lengthBuf := bytes.NewReader(lengthBytes)
-    var msgLen uint32
-    if err = binary.Read(lengthBuf, binary.LittleEndian, &msgLen); err != nil {
-      return nil, err
-    }
-    if msgLen > MAXLEN {
-      holmes.Error("len %d, type %d", msgLen, msgType)
-      return nil, ErrorIllegalData
-    }
-    // read real application message
-    msgBytes := make([]byte, msgLen)
-    _, err = io.ReadFull(c.GetRawConn(), msgBytes)
-    if err != nil {
-      return nil, err
-    }
+	case typeBytes = <-byteChan:
+		typeBuf := bytes.NewReader(typeBytes)
+		var msgType int32
+		if err = binary.Read(typeBuf, binary.LittleEndian, &msgType); err != nil {
+			return nil, err
+		}
+		lengthBytes := make([]byte, NLEN)
+		_, err = io.ReadFull(c.GetRawConn(), lengthBytes)
+		if err != nil {
+			return nil, err
+		}
+		lengthBuf := bytes.NewReader(lengthBytes)
+		var msgLen uint32
+		if err = binary.Read(lengthBuf, binary.LittleEndian, &msgLen); err != nil {
+			return nil, err
+		}
+		if msgLen > MAXLEN {
+			holmes.Error("len %d, type %d", msgLen, msgType)
+			return nil, ErrorIllegalData
+		}
+		// read real application message
+		msgBytes := make([]byte, msgLen)
+		_, err = io.ReadFull(c.GetRawConn(), msgBytes)
+		if err != nil {
+			return nil, err
+		}
 
-    // deserialize message from bytes
-    unmarshaler := GetUnmarshaler(msgType)
-    if unmarshaler == nil {
-      return nil, Undefined(msgType)
-    }
-    return unmarshaler(msgBytes)
-  }
+		// deserialize message from bytes
+		unmarshaler := GetUnmarshaler(msgType)
+		if unmarshaler == nil {
+			return nil, Undefined(msgType)
+		}
+		return unmarshaler(msgBytes)
+	}
 }
 
 // Encode encodes the message into bytes data.
 func (codec TypeLengthValueCodec) Encode(msg Message) ([]byte, error) {
-  data, err := msg.Serialize()
-  if err != nil {
-    return nil, err
-  }
-  buf := new(bytes.Buffer)
-  binary.Write(buf, binary.LittleEndian, msg.MessageNumber())
-  binary.Write(buf, binary.LittleEndian, int32(len(data)))
-  buf.Write(data)
-  packet := buf.Bytes()
-  return packet, nil
+	data, err := msg.Serialize()
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, msg.MessageNumber())
+	binary.Write(buf, binary.LittleEndian, int32(len(data)))
+	buf.Write(data)
+	packet := buf.Bytes()
+	return packet, nil
 }
